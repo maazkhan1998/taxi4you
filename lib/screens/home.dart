3a96d1d408cfef069df33d20c165c1ed1cfcb37a
Mubeen Alam Usmani <57857826+mubeenalam5@users.noreply.github.com>
2021-05-26 12:53:38 +0500

    home updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../utils/SizeConfig.dart';
 
 class HomeScr extends StatefulWidget {
@@ -7,53 +8,218 @@ class HomeScr extends StatefulWidget {
 }
 
 class _HomeScrState extends State<HomeScr> {
+  serviceCard(String image, String service) {
+    return Container(
+      height: ScreenUtil().setHeight(135.0),
+      width: ScreenUtil().setWidth(130.0),
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(color: Colors.grey.shade300, blurRadius: 10.0)
+      ],
+      borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: EdgeInsets.only(top: 4.0, right: 4.0),
+              child: Image.asset(
+                'assets/play.png',height: 22.0,width: 22.0,
+              ),
+            ),
+          ),
+          Image.asset(
+            image,
+            height: 55.0,
+            width: 89.0,
+          ),
+          SizedBox(height: ScreenUtil().setHeight(8.0)),
+          Text(
+            service,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: ScreenUtil().setSp(14.0),
+                color: Colors.black.withOpacity(0.5),
+                fontFamily: 'heavy'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    MySize().init(context);
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            titleSpacing: MySize.size34,
-            title: Text(
-              'Home',
-              style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.white.withOpacity(0.7),
-                  fontFamily: 'medium',
-                  fontWeight: FontWeight.w100),
-            ),
-            actions: [
-              // IconButton(
-              //   icon: Icon(Icons.search,size: 24.0,color: Colors.white.withOpacity(0.7)),
-              //   onPressed: (){}
-              // ),
-              Image.asset(
+        appBar: AppBar(
+          titleSpacing: MySize.size34,
+          title: Text(
+            'Home',
+            style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white.withOpacity(0.9),
+                fontFamily: 'heavy',
+                fontWeight: FontWeight.w400),
+          ),
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child: Image.asset(
                 'assets/search.png',
                 height: 16.0,
                 width: 18.0,
-                color: Colors.white.withOpacity(0.7),
+                color: Colors.white,
               ),
-              SizedBox(width: MySize.size24)
+            ),
+            SizedBox(width: ScreenUtil().setWidth(22.0))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                      height: ScreenUtil().setHeight(165.0),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                'assets/carimg.jpeg',
+                              ),
+                              fit: BoxFit.cover))),
+                  Container(
+                    height: ScreenUtil().setHeight(165.0),
+                    width: double.infinity,
+                    color: Colors.black.withOpacity(0.5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Zuverlässiger Service',
+                          style: TextStyle(
+                              fontFamily: 'heavy',
+                              fontSize: 24.0,
+                              color: Colors.white.withOpacity(0.6)),
+                        ),
+                        SizedBox(
+                          height: ScreenUtil().setHeight(10.0),
+                        ),
+                        Container(
+                          height: ScreenUtil().setHeight(35.0),
+                          width: ScreenUtil().setWidth(265.0),
+                          child: ElevatedButton(
+                            child: Text('ANFRAGE ABSCHICKEN',
+                                style: TextStyle(
+                                    fontFamily: 'medium',
+                                    fontSize: 13.5,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white.withOpacity(0.7))),
+                            style: ElevatedButton.styleFrom(
+                                primary: Color(0xFF3DB1A2)),
+                            onPressed: () {},
+                          ),
+                        ),
+                        SizedBox(
+                          height: ScreenUtil().setHeight(10.0),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: ScreenUtil().setHeight(550.0),
+                width: double.infinity,
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade300,
+                      spreadRadius: 5.0,
+                      blurRadius: 15.0,
+                      offset: Offset(0.0, 3.0))
+                ]),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: ScreenUtil().setHeight(10.0),
+                    ),
+                    Text('Unsere Leistungen',
+                        style: TextStyle(
+                            fontFamily: 'medium',
+                            fontSize: 16.5,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black.withOpacity(0.51))),
+                    SizedBox(
+                      height: ScreenUtil().setHeight(26.0),
+                    ),
+                    Wrap(
+                      alignment: WrapAlignment.center,spacing: 30.0,runSpacing: 28.0,
+                      children: [
+                        serviceCard('assets/page-1 (1).png', 'Schüler Taxi'),
+                        serviceCard(
+                            'assets/page-1 (1).png', 'Behinderte-ntransport'),
+                        serviceCard('assets/page-1(2).png', 'Concierge '),
+                        serviceCard('assets/page-1(3).png', 'Roadshows'),
+                        serviceCard('assets/page-1(4).png', 'Schüler Taxi'),
+                        serviceCard('assets/page-1(5).png', 'Reiseplanung')
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: ScreenUtil().setHeight(20.0),
+              ),
+              Text('Verbreitetes Netzwerk',
+                  style: TextStyle(
+                      fontFamily: 'medium',
+                      fontSize: 16.5,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3DB1A2))),
+              SizedBox(
+                height: ScreenUtil().setHeight(3.0),
+              ),
+              Image.asset(
+                'assets/group-18.png',
+                fit: BoxFit.fitWidth,
+                width: double.infinity,
+                height: ScreenUtil().setHeight(145.0),
+              ),
+              SizedBox(
+                height: ScreenUtil().setHeight(20.0),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 35.0),
+                child: Text(
+                    'Wir verfügen über ein umfangreiches Netzwerk, das wir kontinuierlich ausbauen. Um unseren Kunden auch bei hoher Auslastung die richtige Servicelösung anbieten zu können, tun wir alles dafür.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'medium',
+                        fontSize: 13.5,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black54)),
+              ),
             ],
           ),
-          floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {},
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: BottomAppBar(elevation: 0.5,
-          
-            shape: CircularNotchedRectangle(),
-            notchMargin: 4.0,
-            child: new Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-              
-              ],
-            ),
-          )),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   child: const Icon(Icons.add),
+        //   onPressed: () {},
+        // ),
+        // floatingActionButtonLocation:
+        //     FloatingActionButtonLocation.centerDocked,
+        // bottomNavigationBar: BottomAppBar(
+        //   elevation: 0.5,
+        //   shape: CircularNotchedRectangle(),
+        //   notchMargin: 4.0,
+        //   child: new Row(
+        //     mainAxisSize: MainAxisSize.max,
+        //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //     children: <Widget>[],
+        //   ),
+        // )
+      ),
     );
   }
 }
